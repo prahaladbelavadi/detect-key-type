cb54688eb7f5935f3b6b692847c6302ac1d97a26
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-09-21 14:20:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function check_type(key, format){
+// convert key from input format to buffer or binary array and operate
+
+if (key meets any specific standard){
+    return standard name
+  }
+switch: key
+case1: (key meet standard1){
+  return standard1
+}
+case1: (key meet standard1){
+  return standard1
+}
+  
+}
+
+function check_type_for_array(arr, format){
+  let buffer_array = [];
+  let type_array = []
+  for (let i = 0; i < arr.length; i++){
+    buffer_array.push(convert_formatToBuffer(i))
+  }
+  for (let i = 0; i < buffer_array.length; i++){
+    type_array.push(check_type(i))
+  }
+}
